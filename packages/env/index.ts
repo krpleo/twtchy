1ff3e10c2f7f991b9605a0241ca553d1d4ede409
Kaiden Riley <kaiden.riley@pleo.io>
2024-11-20 23:31:33 +0000

    (feature): improve env error handling

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -1,3 +1,4 @@
+import type {ZodError} from 'zod'
 import {config} from 'dotenv'
 import {expand} from 'dotenv-expand'
 import {z} from 'zod'
@@ -5,13 +6,27 @@ import {z} from 'zod'
 expand(config())
 
 const EnvSchema = z.object({
-  NODE_ENV: z.string().default('development'),
+  NODE_ENV: z.enum(['development', 'production']).default('development'),
+  LOG_LEVEL: z.enum(['fatal', 'error', 'warn', 'trace', 'debug', 'info']).default('info'),
   PORT: z.coerce.number().default(6969),
   TWTCHY_API_KEY: z.string().startsWith('twtchy_'),
   VAL_API_KEY: z.string()
 })
 
-// eslint-disable-next-line node/no-process-env
-const twtchyEnv = EnvSchema.parse(process.env)
+export type TwtchyEnv = z.infer<typeof EnvSchema>
+
+// eslint-disable-next-line import/no-mutable-exports
+let twtchyEnv: TwtchyEnv
+
+try {
+  // eslint-disable-next-line node/no-process-env
+  twtchyEnv = EnvSchema.parse(process.env)
+}
+catch (error) {
+  const e = error as ZodError
+  console.error('❌ Invalid Environment Variables')
+  console.error(e.flatten().fieldErrors)
+  process.exit(1)
+}
 
 export default twtchyEnv
